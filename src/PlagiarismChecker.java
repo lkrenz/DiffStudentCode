c6f85cc4b32c020ca940496d637a309c68d7e692
lkrenz <liamkrenz100@gmail.com>
2025-02-27 21:27:43 -0800

    Worked on understanding the algorithm

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -18,14 +18,19 @@ public class PlagiarismChecker {
      */
     public static int longestSharedSubstring(String doc1, String doc2) {
 
+        // Array to hold the length of paths taken to reach a particular index
         int[][] paths = new int[doc1.length() + 1][doc2.length() + 1];
-        int lowCol = 0;
-        int highest = 0;
+
+        // Adds a place holder space to the front of each string for out of bounds cases
         doc1 = " " + doc1;
         doc2 = " " + doc2;
 
+        // Row major iteration through the 2D array
         for (int i = 1; i < paths.length; i++) {
             for (int j = 1; j < paths[i].length; j++) {
+
+                // At each index, if the current char in both strings match, take the diagonal and add one
+                // Otherwise, take the max of the upward or leftward paths
                 int max = 0;
                 if (doc1.charAt(i) == doc2.charAt(j)) max = paths[i - 1][j-1] + 1;
                 else max = Math.max(paths[i - 1][j], paths[i][j-1]);
